727b87b17945c9622ed2718c6de224933bd611e9
aman-chaturvedi-01 <amanchaturvedi.only1@gmail.com>
2026-09-04 01:27:34 +0530

    Update 0468-max-no.-of-consecutive-1's-in-binary-arr.cpp

--- Arrays/LeetCode/0468-max-no.-of-consecutive-1's-in-binary-arr.cpp
+++ Arrays/LeetCode/0468-max-no.-of-consecutive-1's-in-binary-arr.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0 , max_ones = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                count ++ ;
+                max_ones = max(count,max_ones) ;
+            }
+            else{
+                count = 0 ;
+            }
+        }
+        return max_ones ;
+    }
+};
