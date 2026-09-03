6b8309c0da7726ea923184860205c82d3ef176f5
aman-chaturvedi-01 <amanchaturvedi.only1@gmail.com>
2026-09-03 21:31:34 +0530

    Update find-2nd-Largest-element-from-+ve-array.cpp

--- Arrays/GeeksForGeeks/find-2nd-Largest-element-from-+ve-array.cpp
+++ Arrays/GeeksForGeeks/find-2nd-Largest-element-from-+ve-array.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        int larg = -1 ;
+        int sec_larg = -1 ;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i] > larg){
+                sec_larg = larg ;
+                larg = arr[i] ;
+            }
+            else if(arr[i] > sec_larg && arr[i] < larg){
+                sec_larg = arr[i] ;
+            }
+        }
+        return sec_larg ;
+    }
+};
