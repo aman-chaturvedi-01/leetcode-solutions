94fd65c70f3235d4ceb3bc7f653ab9ea6a6b9e24
aman-chaturvedi-01 <amanchaturvedi.only1@gmail.com>
2026-09-03 22:04:05 +0530

    Update 0268-print-one-missing-number-from-array.cpp

--- Arrays/LeetCode/0268-print-one-missing-number-from-array.cpp
+++ Arrays/LeetCode/0268-print-one-missing-number-from-array.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size() ;
+        int total_sum = (n+1)*(n)/2 ;
+        int arr_sum = 0 ;
+        for(int i=0;i<n;i++){
+            arr_sum += nums[i] ;
+        }
+        return (total_sum - arr_sum) ;
+    }
+};
