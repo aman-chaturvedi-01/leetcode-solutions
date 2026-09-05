35eecc1e00ee4eafe54b07ee8a87af0d6d1c2a21
aman-chaturvedi-01 <amanchaturvedi.only1@gmail.com>
2026-09-06 02:13:56 +0530

    Update 0075-sort-0's-1's-2's-in-non-decresing-order.cpp

--- Arrays/LeetCode/0075-sort-0's-1's-2's-in-non-decresing-order.cpp
+++ Arrays/LeetCode/0075-sort-0's-1's-2's-in-non-decresing-order.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0 , high = nums.size()-1 ;
+        int mid = 0 ;               //current position initially
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low],nums[mid]);
+                low ++ ;
+                mid ++ ;
+            }
+            else if(nums[mid]==1){
+                mid ++ ;
+            }
+            else{                   // nums[mid]==2
+                swap(nums[mid],nums[high]);
+                high -- ;
+            }
+        }
+    }
+};
